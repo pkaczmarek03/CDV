b5abf93a904b7cc8b52cb8a9abb6e98284c4ba98
unknown <panpopcorn99@gmail.com>
2019-04-15 13:18:11 +0200

    a

--- 10_Zasięg _zmiennych.py
+++ 10_Zasięg _zmiennych.py
@@ -5,4 +5,40 @@
 x= "{0:.3f}".format(5)
 print(x)
 
+def plnToChf(value):
+    kursChf = 3.7913
+    iloscChf = value / kursChf
+    iloscChf = "{0:.4f}".format(iloscChf)
+    print(f"Ilosc Chf: {iloscChf}")
+    
+plnToChf(400)
+    
+    
+# uwtwórz fukncje zwracajaca ilosc euro jakią klient chce kupic#
 
+
+def plnToEuro(value):
+    kursEuro = 4.2847
+    iloscEuro = value / kursEuro
+    iloscEuro = "{0:.4f}".format(iloscEuro)
+    print(f"Ilosc Euro: {iloscEuro}")
+    
+
+pln = input()
+euro = plnToEuro(float(pln))
+
+##print(f"Ilosc: {pln}PLN = {euro}Euro")
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
